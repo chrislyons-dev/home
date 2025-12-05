3b31587781f058c3c7edc83b1a53ee718f89ac44
cjlyons <christian.james.lyons@gmail.com>
2025-12-04 18:13:34 -0600

    build: bump react to 19.2.1 for ciritical security fix
    Updated package-lock.json and package.json to:
    - reflect the new version of react
    - update all other dependencies to their latest compatible versions
    
    Written-by: Chris Lyons

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import { visualizer } from 'rollup-plugin-visualizer';
+
+export default {
+  plugins: [
+    visualizer({
+      filename: './docs/architecture/bundle-stats.html',
+      open: false,
+      gzipSize: true,
+      brotliSize: true,
+      template: 'treemap',
+    }),
+  ],
+};
